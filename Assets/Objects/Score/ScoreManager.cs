0e1d63a58316a50e558f5974e0fdd1ce0f3d0ea2
Zixuan James Li <p359101898@gmail.com>
2024-02-06 19:08:05 -0500

    Fix arrow animation and adjust the scene.
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

---
+++ Assets/Objects/Score/ScoreManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int Score { get; private set; }
+    public int Combo { get; private set; }
+
+}
